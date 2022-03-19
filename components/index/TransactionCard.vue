5965c1116a55839deb7ad84de0ec9132393bf7f3
Apanisileik <kolakanmi.apanisile@sterling.ng>
2022-03-19 13:26:14 +0100

    dashboard cards

---
+++ components/index/TransactionCard.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="index-card">
+    <p class="index-card__text u-mb-sm">{{ text }}</p>
+    <h1 class="heading-primary u-text-left">{{ amount }}</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    text: {
+      type: String,
+      default: '',
+    },
+    amount: {
+      type: String,
+      default: '0.00',
+    },
+    theme: {
+      type: String,
+      default: 'dark',
+    },
+  },
+}
+</script>
+
+<style></style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,22 @@
 <template>
-  <div></div>
+  <div class="container">
+    <div class="index-txn-info">
+      <transaction-card
+        text="Total Transaction"
+        amount="N898,000,000"
+      ></transaction-card>
+      <transaction-card text="Total Buy" amount="N1,759,090"></transaction-card>
+      <transaction-card text="Total Sell" amount="N800,000"></transaction-card>
+    </div>
+  </div>
 </template>
 
 <script>
+import TransactionCard from '~/components/index/TransactionCard.vue'
 export default {
+  components: {
+    TransactionCard,
+  },
   layout: 'dashboard',
 }
 </script>
