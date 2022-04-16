fc163f0d4ef02481175b723b055b0dff78e7abc2
Apanisileik <kolakanmi.apanisile@sterling.ng>
2022-04-16 19:43:47 +0100

    password and secret key max width

--- components/settings/PasswordTab.vue
+++ components/settings/PasswordTab.vue
@@ -2,7 +2,7 @@
   <div class="profile profile--min-height-50">
     <h3 class="heading-primary u-white u-mb-10">Change Password</h3>
     <p class="u-mb-40">Enter your current password to change to a new one.</p>
-    <div class="profile__form profile__form--max-w-30">
+    <div class="profile__form profile__form--max-w-40">
       <div class="u-mb-30">
         <p class="u-mb-5">Current Password</p>
         <div class="profile__input-box">
