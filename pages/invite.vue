2cc65b6361f98e1474d7122c3b56e95d33dbef3e
Afekhide Gbadamosi <realafe21@gmail.com>
2022-09-24 22:00:22 +0100

    Update invite.vue

--- pages/invite.vue
+++ pages/invite.vue
@@ -1,6 +1,47 @@
 <template>
-  <div>
-    <tab-holder :tabs="tabs"></tab-holder>
+  <div class="tab-holder" v-if="currentTab">
+    <div class="tab-list">
+      <div class="tab-list__box">
+        <div
+          v-for="(tab, index) in tabs"
+          :key="index"
+          class="tab-list__item"
+          @click="onSelect(tab, index)"
+        >
+          {{ tab.title }}
+        </div>
+        <span
+          class="tab-list__glider"
+          :class="`tab-list__glider--${currentTabIndex + 1}`"
+        ></span>
+      </div>
+    </div>
+    <div class="tab-drop">
+      <p class="tab-drop__title-con" @click="toggleOpen">
+        <span class="title">{{ currentTab.title }}</span>
+        <span class="arrow-con" :class="[dropOpen ? 'rotate-up' : '']"
+        ><img src="/img/awesome-arrow-down-white.svg" alt="down arrow"
+        /></span>
+      </p>
+      <div class="tab-drop__content" :class="[dropOpen ? '' : 'u-none']">
+        <p
+          class="tab-drop__item"
+          v-for="(tab, idx) in tabs"
+          :key="idx"
+          @click="
+            () => {
+              onSelect(tab, idx)
+              dropOpen = false
+            }
+          "
+        >
+          {{ tab.title }}
+        </p>
+      </div>
+    </div>
+    <div>
+      <nuxt-child :referralData="currentTab.properties.referralData" :getReferrals="currentTab.properties.getReferrals" />
+    </div>
   </div>
 </template>
 
