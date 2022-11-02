a076d83aa8af259098f1e8106fc6a06076b50f94
Afekhide Gbadamosi <realafe21@gmail.com>
2022-11-02 22:04:03 +0100

    Update nuxt.config.js

--- nuxt.config.js
+++ nuxt.config.js
@@ -133,7 +133,7 @@ export default {
     },
   ],
 
-  target: 'static',
+  // target: 'static',
   // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
   buildModules: [
     // https://go.nuxtjs.dev/tailwindcss
